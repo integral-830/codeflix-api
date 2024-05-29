cf6d5d63fd01ebf354df303aaa7ec43afdc4b07d
Ayush Verma <av555v5@gmail.com>
2024-05-29 22:06:37 +0530

    Added response wrapper and code refactoring

---
+++ src/main/kotlin/com/codeflix/data/model/ApiResponse.kt
@@ -0,0 +1,20 @@
+package com.codeflix.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CourseResponse (
+    val success:Boolean,
+    val courses : List<Course> = emptyList()
+)@Serializable
+data class FolderResponse (
+    val success:Boolean,
+    val folders : List<Folder> = emptyList()
+)
+
+@Serializable
+data class VideoResponse (
+    val success:Boolean,
+    val videos : List<Video> = emptyList()
+)
+
